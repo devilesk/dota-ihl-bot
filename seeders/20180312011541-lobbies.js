3fda49e8bf4ea0384ed9b8ac03635cb439453635
devilesk <devilesk@gmail.com>
2018-08-07 15:23:59 +0000

    fix seeds

--- seeders/20180312011541-lobbies.js
+++ seeders/20180312011541-lobbies.js
@@ -4,6 +4,7 @@ module.exports = {
     up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Lobbies', [{
         league_id: 1,
         season_id: 1,
+        queue_type: CONSTANTS.QUEUE_TYPE_DRAFT,
         lobby_name: 'funny-yak-74',
         password: 'rd2l',
         state: CONSTANTS.STATE_NEW,
