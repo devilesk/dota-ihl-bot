f90d1548bf6b03ae170fb0a4329257bb65681b11
devilesk <devilesk@gmail.com>
2019-03-21 18:49:28 +0000

    update tests

--- test/int/commands/admin/league.js
+++ test/int/commands/admin/league.js
@@ -5,6 +5,7 @@ const proxyquire = require('proxyquire');
 const path = require('path');
 const sequelizeMockingMocha = require('sequelize-mocking').sequelizeMockingMocha;
 const db = require('../../../../models');
+const client = require('../../../../lib/client');
 const ihlManager = require('../../../../lib/ihlManagerStub');
 const LeagueInfoCommand = proxyquire('../../../../commands/admin/league-info', {
     '../../lib/ihlManager': ihlManager,
@@ -12,6 +13,15 @@ const LeagueInfoCommand = proxyquire('../../../../commands/admin/league-info', {
 const LeagueCreateCommand = proxyquire('../../../../commands/admin/league-create', {
     '../../lib/ihlManager': ihlManager,
 });
+const LeagueSeasonCommand = proxyquire('../../../../commands/admin/league-season', {
+    '../../lib/ihlManager': ihlManager,
+});
+const LeagueUpdateCommand = proxyquire('../../../../commands/admin/league-update', {
+    '../../lib/ihlManager': ihlManager,
+});
+const {
+    findLeague,
+} = require('../../../../lib/db');
 
 describe('League Commands', () => {
     sequelizeMockingMocha(
@@ -30,15 +40,29 @@ describe('League Commands', () => {
         { logging: false },
     );
     
-    const client = {
+    const fakeClient = {
         isOwner: sinon.stub(),
     }
+    let msg;
+    beforeEach(async () => {
+        ihlManager.isMessageFromAdmin.reset();
+        fakeClient.isOwner.reset();
+        msg = {
+            channel: {
+                guild: {
+                    id: '422549177151782925',
+                },
+                send: sinon.stub(),
+            },
+            author: 1,
+            say: sinon.stub(),
+        }
+    });
 
     describe('LeagueInfoCommand', () => {
         let cmd;
         beforeEach(async () => {
-            cmd = new LeagueInfoCommand(client);
-            ihlManager.isMessageFromAdmin.reset();
+            cmd = new LeagueInfoCommand(fakeClient);
         });
         
         it('return hasPermission true', async () => {
@@ -68,44 +92,116 @@ describe('League Commands', () => {
 
     describe('LeagueCreateCommand', () => {
         let cmd;
-        let msg;
         beforeEach(async () => {
-            cmd = new LeagueCreateCommand(client);
-            client.isOwner.reset();
-            msg = {
-                channel: {
-                    guild: {
-                        id: '422549177151782925',
-                    },
-                    send: sinon.stub(),
-                },
-                author: 1,
-                say: sinon.stub(),
-            }
+            cmd = new LeagueCreateCommand(fakeClient);
         });
         
-        it.only('return hasPermission true', async () => {
-            client.isOwner.returns(true);
+        it('return hasPermission true', async () => {
+            fakeClient.isOwner.returns(true);
             assert.isTrue(cmd.hasPermission(msg));
         });
         
-        it.only('return hasPermission false', async () => {
-            client.isOwner.returns(false);
+        it('return hasPermission false', async () => {
+            fakeClient.isOwner.returns(false);
             assert.isFalse(cmd.hasPermission(msg));
         });
         
-        it.only('run create new league', async () => {
+        it('run create new league', async () => {
             ihlManager.getInhouseState.returns(false);
             await cmd.run(msg);
             assert.isTrue(msg.say.calledOnce);
             assert.isTrue(msg.say.calledWith('Inhouse league created.'));
         });
         
-        it.only('run league exists', async () => {
+        it('run league exists', async () => {
             ihlManager.getInhouseState.returns(true);
             await cmd.run(msg);
             assert.isTrue(msg.say.calledOnce);
             assert.isTrue(msg.say.calledWith('Inhouse league already exists.'));
+        });
+    });
+
+    describe('LeagueSeasonCommand', () => {
+        let cmd;
+        beforeEach(async () => {
+            cmd = new LeagueSeasonCommand(fakeClient);
+        });
+        
+        it('return hasPermission true', async () => {
+            ihlManager.isMessageFromAdmin.returns(true);
+            assert.isTrue(cmd.hasPermission(msg));
+        });
+        
+        it('return hasPermission false', async () => {
+            ihlManager.isMessageFromAdmin.returns(false);
+            assert.isFalse(cmd.hasPermission(msg));
+        });
+        
+        it('run create new season', async () => {
+            const league_before = await findLeague(msg.channel.guild.id);
+            await cmd.run(msg);
+            const league_after = await findLeague(msg.channel.guild.id);
+            assert.notEqual(league_before.current_season_id, league_after.current_season_id);
+            assert.isTrue(msg.say.calledOnce);
+            assert.isTrue(msg.say.calledWith('New league season started.'));
+        });
+    });
+
+    describe('LeagueUpdateCommand', () => {
+        let cmd;
+        beforeEach(async () => {
+            cmd = new LeagueUpdateCommand(client);
+        });
+        
+        it('return isValidSetting true', async () => {
+            assert.isTrue(LeagueUpdateCommand.isValidSetting('CaT_eGo_ry_name'));
+        });
+        
+        it('return isValidSetting false', async () => {
+            assert.isFalse(LeagueUpdateCommand.isValidSetting('test'));
+        });
+        
+        it('return hasPermission true', async () => {
+            ihlManager.isMessageFromAdmin.returns(true);
+            assert.isTrue(cmd.hasPermission(msg));
+        });
+        
+        it('return hasPermission false', async () => {
+            ihlManager.isMessageFromAdmin.returns(false);
+            assert.isFalse(cmd.hasPermission(msg));
+        });
+        
+        it('run update league', async () => {
+            const league_before = await findLeague(msg.channel.guild.id);
+            const setting = 'categoryname';
+            const value = 'test';
+            await cmd.run(msg, { setting, value });
+            const league_after = await findLeague(msg.channel.guild.id);
+            assert.notEqual(league_before.category_name, league_after.category_name);
+            assert.isTrue(msg.say.calledOnce);
+            assert.isTrue(msg.say.calledWith(`League setting updated. ${setting} set to ${value}`));
+        });
+        
+        it('run update league', async () => {
+            const league_before = await findLeague(msg.channel.guild.id);
+            const setting = 'categoryname';
+            const value = 'test';
+            await cmd.run(msg, { setting, value });
+            const league_after = await findLeague(msg.channel.guild.id);
+            assert.notEqual(league_before.category_name, league_after.category_name);
+            assert.isTrue(msg.say.calledOnce);
+            assert.isTrue(msg.say.calledWith(`League setting updated. ${setting} set to ${value}`));
+        });
+        
+        it('run update league', async () => {
+            const league_before = await findLeague(msg.channel.guild.id);
+            const setting = 'categoryname';
+            const value = 'test';
+            await cmd.run(msg, { setting, value });
+            const league_after = await findLeague(msg.channel.guild.id);
+            assert.notEqual(league_before.category_name, league_after.category_name);
+            assert.isTrue(msg.say.calledOnce);
+            assert.isTrue(msg.say.calledWith(`League setting updated. ${setting} set to ${value}`));
         });
     });
 });
