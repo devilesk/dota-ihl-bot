a23c25863f8ca4366da50ec2f5389a4ee996f640
devilesk <devilesk@gmail.com>
2019-03-31 23:01:28 +0000

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,18 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "indent": ["error", 4],
+        "class-methods-use-this": ["warn"],
         "camelcase": ["warn"],
         "max-len": ["warn"],
         "brace-style": ["error", "stroustrup"],
         "no-console": ["error", { "allow": ["warn", "error"] }],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
         "prefer-const": ["error", {"destructuring": "all"}],
         "no-await-in-loop": ["warn"],
         "no-restricted-syntax": 0,
         "prefer-destructuring": ["warn"],
         "radix": ["error", "as-needed"],
-        "no-underscore-dangle": 0
+        "no-underscore-dangle": 0,
+        "import/order": 0,
     }
 };
