02bb14ca01dd930b342332b980ef20cc8f5c3fca
devilesk <devilesk@gmail.com>
2019-04-05 12:11:23 +0000

    add set bot status command

---
+++ commands/admin/bot-status.js
@@ -0,0 +1,45 @@
+const IHLCommand = require('../../lib/ihlCommand');
+const CONSTANTS = require('../../lib/constants');
+
+/**
+ * @class BotStatusCommand
+ * @extends IHLCommand
+ */
+module.exports = class BotStatusCommand extends IHLCommand {
+    constructor(client) {
+        super(client, {
+            name: 'bot-status',
+            group: 'admin',
+            memberName: 'bot-status',
+            guildOnly: true,
+            description: 'Manually set a bot status.',,
+            examples: ['bot-status 76561197960287930 BOT_UNAVAILABLE', 'bot-status 76561197960287930 BOT_ONLINE', 'bot-status 76561197960287930 BOT_OFFLINE'],
+            args: [
+                {
+                    key: 'steamid_64',
+                    prompt: 'Provide a bot steam id.',
+                    type: 'string',
+                },
+                {
+                    key: 'status',
+                    prompt: 'Provide a status.',
+                    type: 'string',
+                    validate: (status) => {
+                        if (status.startsWith('BOT_') && Object.prototype.hasOwnProperty.call(CONSTANTS, status)) return true;
+                        return 'Value must be a valid status';
+                    },
+                },
+            ],
+        }, {
+            inhouseAdmin: true,
+            inhouseState: true,
+            lobbyState: false,
+            inhouseUser: false,
+        });
+    }
+
+    async run({ msg, league }, { steamid_64, status }) {
+        this.ihlManager.emit(CONSTANTS.EVENT_BOT_SET_STATUS, league, steamid_64, status);
+        await msg.say(`Bot ${steamid_64} status set to ${status}`);
+    }
+};
