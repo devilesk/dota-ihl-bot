567572c4f20f478ecdcf15dc1f7f92588d9f3ae7
devilesk <devilesk@gmail.com>
2019-04-22 17:46:18 +0000

    updates

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env' });
 
 module.exports = {
-    operatorsAliases: false,
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
