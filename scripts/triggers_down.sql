8d5af038bb7e55d1ae16e3d868ac717fcbe85e42
devilesk <devilesk@gmail.com>
2019-04-27 22:22:43 +0000

    fix: fix triggers_down.sql

--- scripts/triggers_down.sql
+++ scripts/triggers_down.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION IF EXISTS notify_trigger();
 DROP TRIGGER IF EXISTS league_notify ON "Leagues";
 DROP TRIGGER IF EXISTS lobby_notify ON "Lobbies";
 DROP TRIGGER IF EXISTS user_notify ON "Users";
+DROP FUNCTION IF EXISTS notify_trigger();
